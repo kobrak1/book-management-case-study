710b0a993e53f072eb758de35329518476531095
kobrak1 <aburakkarhan@gmail.com>
2024-07-27 12:49:01 +0300

    add: basic configurations were made on app.js

--- api/app.js
+++ api/app.js
@@ -1,2 +1,16 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+// mongodb connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to mongodb'))
+    .catch((error) => console.error('Error connecting to mongodb:', error))
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+module.exports = app
